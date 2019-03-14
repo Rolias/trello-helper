48d73be06e9385e1186926a2780750ca6ba9743e
Tod Gentille <tod.gentille@gmail.com>
2019-03-13 22:13:16 -0700

    remove dead lines

--- main.js
+++ main.js
@@ -1,12 +1,2 @@
-const envCreate = require('env-create')
-// const trello = require('./src/trello_old')
 const Trello = require('./src/trello')
-
-// const auth = (path) => {
-//   envCreate.load({path})
-// }
-// module.exports = {
-//   auth,
-//   trello,
-// }
 module.exports = Trello
